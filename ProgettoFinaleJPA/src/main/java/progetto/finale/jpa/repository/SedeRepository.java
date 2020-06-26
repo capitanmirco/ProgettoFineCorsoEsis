477ed3c7027b7e9d1d564f828a1939ec23d11d30
ambracala <ambracala@mbp-di-ambra.station>
2020-06-26 09:48:49 +0200

    sede rep

---
+++ ProgettoFinaleJPA/src/main/java/progetto/finale/jpa/repository/SedeRepository.java
@@ -0,0 +1,13 @@
+package progetto.finale.jpa.repository; 
+
+import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.stereotype.Repository; 
+
+
+
+import progetto.finale.jpa.entity.Sede; 
+
+@Repository 
+public interface SedeRepository extends JpaRepository{ 
+
+}
