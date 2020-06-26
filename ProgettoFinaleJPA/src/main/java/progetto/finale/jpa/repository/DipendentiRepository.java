c78770c7c3908caba638f141372bea83d1f534e7
mario <quasi.mario@outlook.it>
2020-06-26 09:46:06 +0200

    Aggiornamento dipendentiRepository

--- ProgettoFinaleJPA/src/main/java/progetto/finale/jpa/repository/DipendentiRepository.java
+++ ProgettoFinaleJPA/src/main/java/progetto/finale/jpa/repository/DipendentiRepository.java
@@ -3,10 +3,12 @@ package progetto.finale.jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import progetto.finale.jpa.entity.Dipendenti;
+
 
 
 @Repository
-public interface DipendentiRepository extends JpaRepository<Dipendente,Integer> {
+public interface DipendentiRepository extends JpaRepository<Dipendenti,Integer> {
 
 	//public Dipendente findByidDipendente(int idDipendenti);
 	
