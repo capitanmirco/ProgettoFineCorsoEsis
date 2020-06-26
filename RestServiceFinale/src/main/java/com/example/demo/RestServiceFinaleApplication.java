3637e77f29b210416af7c5cac7079fd34c6bc365
mirco <mirco@tl-wr940n>
2020-06-26 12:17:44 +0200

    update rest app

--- RestServiceFinale/src/main/java/com/example/demo/RestServiceFinaleApplication.java
+++ RestServiceFinale/src/main/java/com/example/demo/RestServiceFinaleApplication.java
@@ -2,8 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
 @SpringBootApplication
+@EnableJpaRepositories("progetto.finale.jpa.respository")
+@ComponentScan({"progetto.finale.jpa"})
+@EntityScan("progetto.finale.jpa.entity")
 public class RestServiceFinaleApplication {
 
 	public static void main(String[] args) {
